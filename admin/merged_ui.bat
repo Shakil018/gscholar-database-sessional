be4c1b07404d3bdf036c9ffb8f5e5fe2c9b2d47a
Shayekh Bin Islam <shikh.bin.islam@gmail.com>
2020-10-31 17:43:28 +0600

    merge command added

---
+++ admin/merged_ui.bat
@@ -0,0 +1,9 @@
+@REM PUll before merging
+git pull
+@REM checkout 
+git checkout shakil
+git merge shayekh
+git checkout -b merged_ui
+@REM rename metrics to _metrics
+@REM shift in vscode
+@REM finally merge git ignore 
